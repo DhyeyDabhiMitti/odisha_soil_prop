af9389eb981c0cef98b67a70bbd708d39cc037f4
DhyeyDabhiMitti <165878126+DhyeyDabhiMitti@users.noreply.github.com>
2024-06-11 17:28:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ def main():
     # Add markers to the map
     for coord in coordinates:
         temp_df = pd.DataFrame(df1[df1['x']==coord['x']][df1['y']==coord['y']])
-        html = temp_df.iloc[:,0:9]).to_html(
+        html = temp_df.iloc[:,0:9].to_html(
                 classes="table table-striped table-hover table-condensed table-responsive"
             )
         popup = folium.Popup(html, max_width=500)
