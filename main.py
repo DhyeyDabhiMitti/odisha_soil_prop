978537129cfe02f09bd0448a945baf5623123212
DhyeyDabhiMitti <165878126+DhyeyDabhiMitti@users.noreply.github.com>
2024-06-20 23:07:58 +0530

    update3

--- main.py
+++ main.py
@@ -4,12 +4,13 @@ from streamlit_folium import st_folium,folium_static
 import pandas as pd
 import geopandas as gpd
 
+st.title("Soil Properties with Marked Coordinates for Orissa")
+
 @st.cache_resource
 def load_data():
     df1 = pd.read_csv('edited_Table1.csv',index_col=0)
     coords = [{'x':row['x'],'y':row['y']} for index,row in df1.iterrows()]
     return coords,df1
-
 
 @st.cache_resource
 def main():
@@ -52,24 +53,22 @@ def main():
         ).add_to(m)
     '''
     return m
-
-if __name__ == '__main__':   
-    st.title("Soil Properties with Marked Coordinates for Orissa")
-    st.write("starts")
-    if 'map' not in st.session_state:
-        st.write("in if")
-        map = main()
-        st.write("main executed")
-        st.session_state['map'] = map
-        gdf = gpd.read_file('odisha.geojson')
-        gdf = gdf.iloc[:10,:]
-        fg = folium.FeatureGroup(name="Districts",show=False)
-        for index,row in gdf.iterrows():
-            temp_poly = row['geometry']
-            fg.add_child(folium.GeoJson(temp_poly))
-        st.session_state['fg'] = fg
-    st.write('if completed')
-    data = st_folium(st.session_state['map'],width=800, height=500,feature_group_to_add=st.session_state['fg'],layer_control=folium.LayerControl())
-    st.write("map displayed")
-    st.write(data)
+  
+st.write("starts")
+if 'map' not in st.session_state:
+    st.write("in if")
+    map = main()
+    st.write("main executed")
+    st.session_state['map'] = map
+    gdf = gpd.read_file('odisha.geojson')
+    gdf = gdf.iloc[:10,:]
+    fg = folium.FeatureGroup(name="Districts",show=False)
+    for index,row in gdf.iterrows():
+        temp_poly = row['geometry']
+        fg.add_child(folium.GeoJson(temp_poly))
+    st.session_state['fg'] = fg
+st.write('if completed')
+data = st_folium(st.session_state['map'],width=800, height=500,feature_group_to_add=st.session_state['fg'],layer_control=folium.LayerControl())
+st.write("map displayed")
+st.write(data)
 
