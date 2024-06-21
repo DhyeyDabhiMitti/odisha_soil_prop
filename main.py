4ab7e11456287c0714bd70eb7a0c361da8ac9a42
DhyeyDabhiMitti <165878126+DhyeyDabhiMitti@users.noreply.github.com>
2024-06-21 16:13:16 +0530

    update done

---
+++ main.py
@@ -0,0 +1,74 @@
+import streamlit as st
+import folium
+from streamlit_folium import st_folium,folium_static
+import pandas as pd
+import geopandas as gpd
+import numpy as np
+
+st.title("Soil Properties with Marked Coordinates for Orissa")
+
+@st.cache_resource
+def load_data():
+    df1 = pd.read_csv('edited_Table1.csv',index_col=0)
+    coords = [{'x':row['x'],'y':row['y']} for index,row in df1.iterrows()]
+    return coords,df1
+
+@st.cache_resource
+def main():
+
+    # Define coordinates
+    coordinates,df1 = load_data()
+    print(len(coordinates),df1.shape)
+
+    # Create a Folium map centered around the first location
+    cent_X = 0
+    cent_Y = 0
+    for coords in coordinates:
+        cent_X+=coords['x']
+        cent_Y+=coords['y']
+    cent_X = cent_X/len(coordinates)
+    cent_Y = cent_Y/len(coordinates)
+    map_center = (cent_X, cent_Y)
+
+    #Create the folium map
+    m = folium.Map(location=map_center, zoom_start=5)
+
+    # Add district layer
+    gdf = gpd.read_file('odisha.geojson')
+    for index,row in gdf.iterrows():
+        temp_poly = row['geometry']
+        m.add_child(folium.GeoJson(temp_poly))
+
+    #folium.LayerControl().add_to(m)
+
+    # Add markers to the map
+    
+    for coord in coordinates:
+        temp_df = pd.DataFrame(df1[df1['x']==coord['x']][df1['y']==coord['y']])
+        html = temp_df.iloc[:,0:9].to_html(
+                classes="table table-striped table-hover table-condensed table-responsive"
+            )
+        popup = folium.Popup(html, max_width=500)
+        folium.Marker(
+            location=(coord["x"], coord["y"]),
+            tooltip = str(coord['x']) + ' , ' + str(coord['y']),
+            popup = popup
+        ).add_to(m)
+
+    return m,df1
+
+if 'map' not in st.session_state:
+    st.session_state['map'],st.session_state['df'] = main()
+folium_static(st.session_state['map'],width=800, height=500)
+
+col1,col2 = st.columns(2)
+
+with col1:
+    x = float(st.text_input("Latitude: ",value=0))
+    y = float(st.text_input("Longitude: ",value=0))
+df = st.session_state['df']
+x_check = np.isclose(df.x,x)
+y_check = np.isclose(df.y,y)
+filtered_df = df[np.logical_and(x_check,y_check)]
+st.table(filtered_df)
+st.download_button("Download data in CSV format",filtered_df.to_csv().encode("utf-8"),file_name="Data_Table1"+str(x)+' '+str(y)+".csv",mime="text/csv")
